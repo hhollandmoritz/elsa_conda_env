8d2579e3b56b02a1d74289f312ad2c81968494ab
Hannah Holland-Moritz <hhollandmoritz@gmail.com>
2022-10-31 14:58:50 -0400

    updating installation from template so it actually installs ELSA

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # elsa_conda_env
 creating a conda environment to facilitate installation of elsa program: https://bitbucket.org/charade/elsa/src/master/
+
+## Create conda environment
+
+``` bash
+git clone https://github.com/hhollandmoritz/elsa_conda_env.git
+cd elsa_conda_env
+./install_elsa.sh # this installs the conda environment, ELSA, and tests the installation
+conda activate elsa_env_vX
+```
+
+## To run elsa
+
+Activate the conda environment
+``` bash
+conda activate elsa_env_vX
+```
+
+
---
+++ install_elsa.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install the conda environment:
+conda env create -f elsa_install.yml --force
+
+source $(conda info --base)/etc/profile.d/conda.sh
+# Since the name changes with each version, make sure to pull the version
+# name out of the yml file
+ENV_NAME=$(awk '/name: elsa_env_v/{print $NF}' elsa_install.yml)
+conda activate $ENV_NAME
+
+# Download ELSA and unzip
+echo "Downloading ELSA master branch"
+wget https://bitbucket.org/charade/elsa/get/master.tar.gz
+tar -zxvf charade-elsa-master.tar.gz
+
+cd charade-elsa*
+
+# Install ELSA
+echo "Installing ELSA"
+python setup.py install
+
+# Test installation
+echo "Testing installation"
+cd test
+./test.sh
+ 
+# Deactivate the conda environment
+conda deactivate
